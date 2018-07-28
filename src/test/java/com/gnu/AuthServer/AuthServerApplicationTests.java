377a5b0675df8c76d4551e9556a13d3eca2f8eb4
GNU Shim <geunwoo.j.shim@gmail.com>
2018-07-28 09:59:26 +0900

    password_grant unit test

--- src/test/java/com/gnu/AuthServer/AuthServerApplicationTests.java
+++ src/test/java/com/gnu/AuthServer/AuthServerApplicationTests.java
@@ -13,13 +13,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.http.MediaType;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.FilterChainProxy;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
@@ -33,10 +33,16 @@ import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import com.gnu.AuthServer.dto.AccessToken;
 
 @RunWith(SpringRunner.class)
-@WebAppConfiguration
-@SpringBootTest(classes=AuthServerApplication.class)
+@SpringBootTest(webEnvironment=WebEnvironment.RANDOM_PORT, classes=AuthServerApplication.class)
 public class AuthServerApplicationTests {
-	Logger logger = LoggerFactory.getLogger(AuthServerApplicationTests.class);
+	private static final Logger LOG = LoggerFactory.getLogger(AuthServerApplicationTests.class);
+	private static final String CLIENT_SECRET = "gnu-secret";
+	private static final String CLIENT_ID = "gnu-gnu";
+	private static final String CALLBACK_URL = "http://localhost:7077/resources/open/callback";
+	private static final String REQUEST_TOKEN_ENDPOINT = "/oauth/authorize";
+	private static final String ACCESS_TOKEN_ENDPOINT = "/oauth/token";
+	private static final String LOGIN_ENDPOINT = "/login";
+
 
 	@Autowired
 	private WebApplicationContext ctx;
@@ -53,7 +59,7 @@ public class AuthServerApplicationTests {
 	
 	@Before
 	public void init() throws Exception {
-		logger.info("----- unit test initialize");
+		LOG.info("----- unit test initialize");
 		username = "gnu";
 		password = "pass";
 		this.mvc = MockMvcBuilders.webAppContextSetup(ctx).addFilters(filterChainProxy).build();
@@ -63,49 +69,70 @@ public class AuthServerApplicationTests {
 	@Test
 	public void testGetRequestToken() throws Exception {
 		testFormLogin();
-		String requestToken = getRequestToken();
+		String requestToken = getRequestTokenForAuthorizationCode();
 		assertThat(requestToken).isNotEmpty();
-		assertThat(getAccessToken(requestToken)).isNotNull();
+		assertThat(getAccessTokenForAuthorizationCode(requestToken)).isNotNull();
+		assertThat(getAccessTokenForPassword()).isNotNull();
 	}
 	
 	private void testFormLogin() throws Exception {
-		logger.info("----- form login test");
-		ResultActions result = mvc.perform(formLogin("/login").user(username).password(password));
+		LOG.info("----- form login test");
+		ResultActions result = mvc.perform(formLogin(LOGIN_ENDPOINT).user(username).password(password));
 		assertThat(result.andReturn().getResponse().getStatus()).isNotSameAs(500);
 	}
 	
-	private AccessToken getAccessToken(String requestToken) throws Exception{
-		logger.info("----- access token test");
-		MultiValueMap<String, String> request = new LinkedMultiValueMap<>();
-		request.add("grant_type", "authorization_code");
-		request.add("code", requestToken);
-		request.add("redirect_uri", "http://localhost:7077/resources/open/callback");
-		request.add("client_id", "gnu-gnu");
-		request.add("client_secret", "gnu-secret");
+	private AccessToken getAccessTokenForAuthorizationCode(String requestToken) throws Exception{
+		LOG.info("----- authorization_code access token test");
+		MultiValueMap<String, String> requestParam = new LinkedMultiValueMap<>();
+		requestParam.add("grant_type", "authorization_code");
+		requestParam.add("code", requestToken);
+		requestParam.add("redirect_uri", CALLBACK_URL);
+		requestParam.add("client_id", CLIENT_ID);
+		requestParam.add("client_secret", CLIENT_SECRET);
 		/* 
 		 * /oauth/token endpoint의 경우 ClientCredentialsTokenEndpointFilter가 등록되어 있음.
 		 * client_id, client_secret을 이용하여 Authentication을 수행하므로 username, password 기반 토큰을 엔드포인트 인증을 위해 끼워넣을 경우 오히려 에러가 발생함 
 	     */ 
-		MockHttpServletRequestBuilder reqBuilder = post("/oauth/token").params(request).contentType(MediaType.APPLICATION_FORM_URLENCODED).accept(MediaType.APPLICATION_JSON_UTF8).characterEncoding("UTF-8");
+		MockHttpServletRequestBuilder reqBuilder = post(ACCESS_TOKEN_ENDPOINT).params(requestParam).contentType(MediaType.APPLICATION_FORM_URLENCODED).accept(MediaType.APPLICATION_JSON_UTF8).characterEncoding("UTF-8");
 		ResultActions result = mvc.perform(reqBuilder).andExpect(status().isOk());
 		String responseBody = result.andReturn().getResponse().getContentAsString();
 		ObjectMapper mapper = new ObjectMapper();
 		mapper.setPropertyNamingStrategy(PropertyNamingStrategy.SNAKE_CASE);
 		AccessToken accessToken = mapper.readValue(responseBody, AccessToken.class);
-		logger.info("raw response : {}", responseBody);
-		logger.info("Access token dto : {}", accessToken.toString());
+		LOG.info("raw response : {}", responseBody);
+		LOG.info("Access token dto : {}", accessToken.toString());
 		return accessToken;
 	}
 	
-	private String getRequestToken() throws Exception {
-		logger.info("----- request token test");
-		MultiValueMap<String, String> request = new LinkedMultiValueMap<>();
-		request.add("response_type", "code");
-		request.add("redirect_uri", "http://localhost:7077/resources/open/callback");
-		request.add("client_id", "gnu-gnu");
-		request.add("client_secret", "gnu-secret");
-		request.add("scope", "read+check");
-		MockHttpServletRequestBuilder reqBuilder = post("/oauth/authorize").with(authentication(auth)).params(request).contentType(MediaType.APPLICATION_FORM_URLENCODED).accept(MediaType.APPLICATION_JSON_UTF8).characterEncoding("UTF-8");
+	private AccessToken getAccessTokenForPassword() throws Exception {
+		LOG.info("----- password grant access token test");
+		MultiValueMap<String, String> requestParam = new LinkedMultiValueMap<>();
+		requestParam.add("grant_type", "password");
+		requestParam.add("client_id", CLIENT_ID);
+		requestParam.add("client_secret", CLIENT_SECRET);
+		requestParam.add("scope", "read+check");
+		requestParam.add("username", username);
+		requestParam.add("password", password);
+		MockHttpServletRequestBuilder reqBuilder = post(ACCESS_TOKEN_ENDPOINT).params(requestParam).contentType(MediaType.APPLICATION_FORM_URLENCODED).accept(MediaType.APPLICATION_JSON_UTF8).characterEncoding("UTF-8");
+		ResultActions result = mvc.perform(reqBuilder).andExpect(status().isOk());
+		String responseBody = result.andReturn().getResponse().getContentAsString();
+		ObjectMapper mapper = new ObjectMapper();
+		mapper.setPropertyNamingStrategy(PropertyNamingStrategy.SNAKE_CASE);
+		AccessToken accessToken = mapper.readValue(responseBody, AccessToken.class);
+		LOG.info("raw response : {}", responseBody);
+		LOG.info("Access token dto : {}", accessToken.toString());
+		return accessToken;
+	}
+	
+	private String getRequestTokenForAuthorizationCode() throws Exception {
+		LOG.info("----- request token test");
+		MultiValueMap<String, String> requestParam = new LinkedMultiValueMap<>();
+		requestParam.add("response_type", "code");
+		requestParam.add("redirect_uri", CALLBACK_URL);
+		requestParam.add("client_id", CLIENT_ID);
+		requestParam.add("client_secret", CLIENT_SECRET);
+		requestParam.add("scope", "read+check");
+		MockHttpServletRequestBuilder reqBuilder = post(REQUEST_TOKEN_ENDPOINT).with(authentication(auth)).params(requestParam).contentType(MediaType.APPLICATION_FORM_URLENCODED).accept(MediaType.APPLICATION_JSON_UTF8).characterEncoding("UTF-8");
 		ResultActions result = mvc.perform(reqBuilder).andExpect(status().is3xxRedirection());
 		return result.andReturn().getResponse().getRedirectedUrl().split("\\?code=")[1];
 	}
